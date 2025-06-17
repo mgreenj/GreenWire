1cd42e8a7882e6de0849f7d561363d5eab75509f
Maurice <maurice.green0740@gmail.com>
2025-06-16 20:03:07 -0400

    updated instructions; build script

--- docs/appsetup.md
+++ docs/appsetup.md
@@ -1,15 +1,37 @@
 
 # Setting up the Application
 
-## Pull the Repository
+Please note that the cross compile setup script was developed to run on Macos.  If you're cross-compiling from a windows machine (i.e. mingw-64), you will have to modify it appropriately.  Below is an example of a change that would be required:
+
 ```
-git clone https://
+# Every instance of `make -j`
+
+# from
+make -j$(sysctl -n hw.ncpu)
+
+# to
+make -j$(nproc)
 ```
 
-## Run the setup Script
+## Pull the Repository
+```
+git clone https://github.com/mgreenj/GreenWire.git
+```
+
+## Clean Shell Environment
+```
+env -i bash --noprofile --norc
+
+export HOME=/Users/0x0m03ii
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin
+export TERM=xterm-256color
+```
+
+## Run the Cross-Compile setup Script
 ```
 cd GreenWire
 chmod +x scripts/setup_crosscompiler.sh
+./scripts/setup_crosscompiler.sh
 ```
 
 ## Compile using Cross-compiler Environment Provided
--- scripts/setup_crosscompiler.sh
+++ scripts/setup_crosscompiler.sh
@@ -4,6 +4,7 @@
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
 MAGENTA="\033[1;35m"
 BOLD="\033[1m"
 RESET="\033[0m"
@@ -29,15 +30,18 @@ done
 
 echo -e "${GREEN}${BOLD}[+] Moving into sysroot and extracting Linux headers and libc...${RESET}"
 cd $SYSROOT
-mkdir -p debs && cd debs
 
 wget -q https://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-libc-dev_6.14.0-22.22_amd64.deb
 wget -q https://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-6.14.0-17_6.14.0-17.17_all.deb
 wget -q https://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-6.14.0-22-generic_6.14.0-22.22_amd64.deb
+wget -q http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev_2.39-0ubuntu8_amd64.deb
+wget -q http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-14/libgcc-14-dev_14.3.0-1ubuntu1_amd64.deb
 
 ar x linux-libc-dev_*.deb && tar -xf data.tar.*
 ar x linux-headers-6.14.0-17_*.deb && tar -xf data.tar.*
 ar x linux-headers-6.14.0-22-generic_*.deb && tar -xf data.tar.*
+ar x libc6-dev_*.deb && tar -xf data.tar.*
+ar x libgcc-14-dev_*.deb && tar -xf data.tar.*
 
 cd $PROJECTROOT/src
 
@@ -57,6 +61,7 @@ cd $PROJECTROOT/build/binutils
     --disable-werror \
     --with-system-zlib
 
+echo -e "${MAGENTA}${BOLD}[+] Running make and make install for Binutils $BINUTILS_VER...${RESET}"
 make -j$(sysctl -n hw.ncpu) V=1 > build.log 2>&1
 make install
 
@@ -72,6 +77,8 @@ cd gcc-$GCC_VER
 echo -e "${GREEN}${BOLD}[+] Configuring and building GCC (bootstrap)...${RESET}"
 cd $PROJECTROOT/build/gcc-bootstrap
 
+SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+
 ../../src/gcc-$GCC_VER/configure \
     --target=$TARGET \
     --prefix=$PREFIX \
@@ -81,23 +88,77 @@ cd $PROJECTROOT/build/gcc-bootstrap
     --disable-shared \
     --disable-threads \
     --disable-multilib \
-    --enable-languages=c
+    --disable-bootstrap \
+    --disable-libsanitizer \
+    --enable-languages=c \
+    --with-system-zlib
 
-make all-gcc -j$(sysctl -n hw.ncpu)
-make install-gcc
+
+echo -e "${MAGENTA}${BOLD}[+] Running make all-gcc for GCC $GCC_VER...${RESET}"
+if ! make all-gcc -j$(sysctl -n hw.ncpu) > build.log 2>&1; then
+    echo -e "${RED}${BOLD}[!] Bootstrap GCC build failed. See build.log for details.${RESET}"
+    exit 1
+fi
+
+echo -e "${MAGENTA}${BOLD}[+] Running make install-gcc for GCC $GCC_VER...${RESET}"
+if ! make install-gcc >> build.log 2>&1; then
+    echo -e "${RED}${BOLD}[!] Bootstrap GCC install failed. See build.log for details.${RESET}"
+    exit 1
+fi
 
 echo -e "${GREEN}${BOLD}[+] Configuring and building GCC (final with sysroot)...${RESET}"
 cd $PROJECTROOT/build/gcc-final
 
+export PATH="$PREFIX/bin:$PATH"
+export CC="$PREFIX/bin/$TARGET-gcc"
+export CXX="$PREFIX/bin/$TARGET-g++"
+export AR="$PREFIX/bin/$TARGET-ar"
+export AS="$PREFIX/bin/$TARGET-as"
+export RANLIB="$PREFIX/bin/$TARGET-ranlib"
+export LD="$PREFIX/bin/$TARGET-ld"
+export STRIP="$PREFIX/bin/$TARGET-strip"
+export NM="$PREFIX/bin/$TARGET-nm"
+export CFLAGS_FOR_TARGET="--sysroot=$SYSROOT"
+export CXXFLAGS_FOR_TARGET="--sysroot=$SYSROOT"
+export LDFLAGS_FOR_TARGET="--sysroot=$SYSROOT"
+
+echo -e "${BLUE}${BOLD}[+] Copying target files to correct location...${RESET}"
+ln -sfn ../usr/lib/x86_64-linux-gnu $SYSROOT/lib/x86_64-linux-gnu
+
+# echo -e "${YELLOW}${BOLD}[+] Debug: Testing where ld is looking...${RESET}"
+# echo 'int main() { return 0; }' > $PROJECTROOT/test.c
+# $PREFIX/bin/$TARGET-gcc -v -Wl,--verbose $PROJECTROOT/test.c -o $PROJECTROOT/test.out 2>&1 | tee $PROJECTROOT/linker_debug.log
+
+# print env to terminal for troubleshooting
+env 
+
+echo -e "${BLUE}${BOLD}[+] Configuring gcc-final $GCC_VER...${RESET}"
 ../../src/gcc-$GCC_VER/configure \
-    --target=$TARGET \
-    --prefix=$PREFIX \
-    --with-sysroot=$SYSROOT \
-    --enable-languages=c,c++ \
-    --disable-nls \
-    --disable-multilib
+  --target=$TARGET \
+  --prefix=$PREFIX \
+  --with-sysroot=$SYSROOT \
+  --with-native-system-header-dir=/usr/include \
+  --with-glibc-version=2.39 \
+  --with-gxx-include-dir=/usr/include/c++/$GCC_VER \
+  --with-multilib-list= \
+  --enable-languages=c,c++ \
+  --disable-nls \
+  --disable-multilib \
+  --disable-bootstrap \
+  --disable-libsanitizer \
+  --with-libdir=lib/x86_64-linux-gnu
 
-make -j$(sysctl -n hw.ncpu)
-make install
 
-echo -e "${GREEN}${BOLD}✅ Cross-compilation toolchain successfully built and installed to $PREFIX${RESET}"
+echo -e "${MAGENTA}${BOLD}[+] Running make for GCC $GCC_VER...${RESET}"
+if ! make -j$(sysctl -n hw.ncpu) V=1 > build.log 2>&1; then
+    echo -e "${RED}${BOLD}[!] Final GCC build failed. See build.log for details.${RESET}"
+    exit 1
+fi
+
+echo -e "${MAGENTA}${BOLD}[+] Running make install for GCC $GCC_VER...${RESET}"
+if ! make install >> build.log 2>&1; then
+    echo -e "${RED}${BOLD}[!] Final GCC install failed. See build.log for details.${RESET}"
+    exit 1
+fi
+
+echo -e "${GREEN}${BOLD} Cross-compilation toolchain successfully built and installed to $PREFIX${RESET}"
