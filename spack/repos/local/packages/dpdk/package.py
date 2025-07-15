236414c73437f467ad40bd9e742effe60e7b770c
Maurice <maurice.green0740@gmail.com>
2025-07-14 23:33:05 -0400

    change hash

--- spack/repos/local/packages/dpdk/package.py
+++ spack/repos/local/packages/dpdk/package.py
@@ -11,7 +11,7 @@ class Dpdk(MesonPackage):
     maintainers = ['mgreenj']
 
     version("25.03", sha256="6a40a731328286ebd79685b18ffa5992e6b770c13843d65fd0d1157cfccff63d")
-    version("23.03", sha256="922067ad6b0a0daf934adf833fcdc29c5154837a4a1f6dc0ffbdcf6dab9edef5")
+    version("23.03", sha256="8a8fa67941b1e0d428937f9068f401457e4e4fd576031479450da065385b332c")
 
 
     depends_on("cmake@3.16.9", type="build")
