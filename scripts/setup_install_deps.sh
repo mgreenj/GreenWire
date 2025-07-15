a4a8f26430d118d23dd794c4ddfac247e0b1b427
Maurice <maurice.green0740@gmail.com>
2025-07-14 22:59:30 -0400

    fix spack.yaml

--- scripts/setup_install_deps.sh
+++ scripts/setup_install_deps.sh
@@ -8,8 +8,8 @@ echo "[+] Installing system build dependencies..."
 sudo apt install -y \
   build-essential \
   python3-pip \
-  # libnuma-dev \
   libbpf-dev
+  # libnuma-dev \
 
 echo "[+] Clearning Apt packages..."
-sudo apt autoremov
+sudo apt autoremove
